b77b3d266a9c39ab8801cd53afdb3761c4261d45
Ilker Sener <eilkersener@gmail.com>
2022-08-15 15:05:20 +0300

    v2

--- app.js
+++ app.js
@@ -3,11 +3,26 @@ const app = express();
 
 const port = 3000;
 
+//Template Engine
+app.set("view engine", "ejs");
 
-app.get('/',(req,res)=>{
-    res.send('index sayfası')
-})
+//MIDDLEWAREs
 
-app.listen(port,(req,res)=>{
-    console.log(`App started on port ${port} `)
-})
+app.use(express.static("public"));
+
+
+app.get('/', (req, res) => {
+  res.status(200).render('index', {
+    page_name : "index"
+  })
+});
+
+app.get('/about', (req, res) => {
+  res.status(200).render('about', {
+    page_name : "about"
+  })
+});
+
+app.listen(port, (req, res) => {
+  console.log(`App started on port ${port} `);
+});
